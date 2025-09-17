8da7bfe9bfc3fade670025d9ecbad1ba8192c31e
Kachi <onyekansirimobi16@gmail.com>
2025-09-18 00:24:31 +0100

    Fonts and Colors

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -45,11 +45,11 @@ const Testimonials = () => {
 
   return (
     <section id='Testimonials'
-    className="w-full flex justify-center items-center bg-white text-black relative overflow-hidden px-8 py-8 md:py-12">
+    className="w-full flex justify-center items-center bg-white text-black relative overflow-hidden px-8 py-8 md:py-16">
         <div className="flex w-full max-w-7xl flex-col md:flex-row items-start justify-between gap-6 md:gap-10">
             
             {/* Left Section */}
-            <div className="w-full md:w-1/4 flex flex-col gap-3 md:gap-6">
+            <div className="w-full md:w-1/4 flex flex-col gap-3 md:gap-6 lg:gap-8">
                 <motion.h1 
                 initial={{ opacity: 0, y: -100 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -71,7 +71,7 @@ const Testimonials = () => {
 
             {/* Testimonials Grid */}
             <motion.div
-            className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-8 flex-1"
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-8 lg:gap-12 flex-1"
             variants={containerVariants}
             initial="hidden"
             animate="visible"
@@ -84,8 +84,8 @@ const Testimonials = () => {
                 variants={itemVariants}
                 exit={{ opacity: 0, x: -80 }}
 
-                className='flex flex-col rounded-2xl overflow-hidden shadow-md shadow-[#000000]/40 min-h-[150px] md:min-h-[250px]'> 
-                    <div className='text-black p-4 md:p-4 text-center flex-grow text-sm md:text-md'>
+                className='flex flex-col rounded-2xl overflow-hidden shadow-md shadow-[#000000]/40 min-h-[150px] md:min-h-[260px]'> 
+                    <div className='text-black p-4 md:p-4 text-center flex-grow text-md'>
                         <p>{test.review}</p>
                     </div>
                     <div className='bg-[#4C86C4] text-black flex flex-row items-center gap-4 p-2 md:p-4'>
