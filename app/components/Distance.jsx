2ee0c2e0138301303d7ca05bf9e205cb6ea140e6
Kachi <onyekansirimobi16@gmail.com>
2025-09-21 19:15:26 +0100

    Testimonials

--- app/components/Distance.jsx
+++ app/components/Distance.jsx
@@ -24,7 +24,7 @@ const Distance = () => {
           viewport={{ once: false, amount: 0.3 }} 
           exit={{ opacity: 0, x: 100 }}
           transition={{ duration: 1.2 }}
-          className="relative w-60 h-60 md:w-90 md:h-90">
+          className="relative w-60 h-60 md:w-86 md:h-86">
           <Image src={assets.location} alt="location" fill className="object-contain"/>
         </motion.div>
 
@@ -40,7 +40,7 @@ const Distance = () => {
           viewport={{ once: false, amount: 0.3 }} 
           exit={{ opacity: 0, x: -100 }}
           transition={{ duration: 1.2 }}
-          className="relative w-60 h-60 md:w-90 md:h-90">
+          className="relative w-60 h-60 md:w-86 md:h-86">
           <Image src={assets.nylon} alt="nylon" fill className="object-contain"/>
         </motion.div>
 
